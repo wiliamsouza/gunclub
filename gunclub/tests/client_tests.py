0166affcc30b8969220669bc05c85ee544c1dfc6
Wiliam Souza <wiliamsouza83@gmail.com>
2012-08-23 21:52:09 -0300

    Added new tests

--- gunclub/tests/client_tests.py
+++ gunclub/tests/client_tests.py
@@ -1,29 +1,26 @@
 from django.test import TestCase
 
 
-class LoginTest(TestCase):
+class ClientTest(TestCase):
     fixtures = ['users_data.json']
 
-    @classmethod
-    def setUpClass(cls):
-        super(LoginTest, cls).setUpClass()
-
-    @classmethod
-    def tearDownClass(cls):
-        super(LoginTest, cls).tearDownClass()
-
-    def test_redirect_to_login_home(self):
+    def test_login_required_home(self):
         response  = self.client.get('/', follow=True)
         self.assertRedirects(response, '/accounts/login/?next=/')
 
-    def test_redirect_to_login_dasboard_admin(self):
+    def test_login_required_admin_dasboard(self):
         response = self.client.get('/dashboard/admin/', follow=True)
         self.assertRedirects(
             response, '/accounts/login/?next=/dashboard/admin/'
             )
 
-    def test_redirect_to_login_dashboard_user(self):
+    def test_login_required_user_dashboard(self):
         response = self.client.get('/dashboard/user/', follow=True)
         self.assertRedirects(
             response, '/accounts/login/?next=/dashboard/user/'
             )
+
+    def test_not_staff_forbidden_admin_dashboard(self):
+        self.client.login(username='user', password='secret')
+        response = self.client.get('/dashboard/admin/')
+        self.assertEqual(response.status_code, 403)
