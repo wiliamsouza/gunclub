e7121b6996699bf18f556c87fa39fbdf92bf59ed
Wiliam Souza <wiliamsouza83@gmail.com>
2012-08-28 20:55:20 -0300

    Added dashboard to installed apps

--- gunclub/gunclub/settings.py
+++ gunclub/gunclub/settings.py
@@ -125,8 +125,9 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'registration',
-    'member',
     'django_nose',
+    'member',
+    'dashboard',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
