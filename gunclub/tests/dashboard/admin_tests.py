5418d4a342b8071a5c1e3f6ef8ae676f9e7219d4
Wiliam Souza <wiliamsouza83@gmail.com>
2012-08-28 20:24:54 -0300

    Added dashboard tests

---
+++ gunclub/tests/dashboard/admin_tests.py
@@ -0,0 +1,23 @@
+""" Admin dashboard tests.
+"""
+from django.test import TestCase
+
+
+class AdminTest(TestCase):
+    fixtures = ['users_data.json']
+
+    def test_admin_dashboard(self):
+        self.client.login(username='admin', password='secret')
+        response = self.client.get('/dashboard/admin/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.context['members']), 2)
+        self.assertEqual(response.context['title'], 'Admin dashboard')
+        self.assertTrue('form' in response.context)
+
+    def test_user_dashboard(self):
+        self.client.login(username='user', password='secret')
+        response = self.client.get('/dashboard/user/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.context['members']), 2)
+        self.assertEqual(response.context['title'], 'User dashboard')
+        self.assertTrue('form' in response.context)
