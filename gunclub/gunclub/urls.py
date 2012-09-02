bf54727643e0f06523c4e82016d21d3b1093c813
Wiliam Souza <wiliamsouza83@gmail.com>
2012-09-02 01:27:38 -0300

    Added add_member view

--- gunclub/gunclub/urls.py
+++ gunclub/gunclub/urls.py
@@ -2,12 +2,13 @@ from django.conf.urls import patterns, include, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from dashboard.views import home, user_dashboard, admin_dashboard
-
+from member.views import add_member
 
 urlpatterns = patterns('',
     url(r'^accounts/', include('registration.backends.default.urls')),
     url(r'^$', home, name='home'),
-    url(r'^dashboard/user/', user_dashboard, name='user_dashboard'),
-    url(r'^dashboard/admin/', admin_dashboard, name='admin_dashboard'),
+    url(r'^dashboard/user/$', user_dashboard, name='user_dashboard'),
+    url(r'^dashboard/admin/$', admin_dashboard, name='admin_dashboard'),
+    url(r'^member/add/$', add_member, name='add_member')
 )
 urlpatterns += staticfiles_urlpatterns()
