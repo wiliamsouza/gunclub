ab216b45b6ada04e09303be1ca985ef26117075d
Wiliam Souza <wiliamsouza83@gmail.com>
2012-09-20 17:11:30 -0300

    Added member profile detail view

--- gunclub/gunclub/urls.py
+++ gunclub/gunclub/urls.py
@@ -1,8 +1,12 @@
 from django.conf.urls import patterns, include, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
+from django.views.generic import DetailView
+
 from dashboard.views import home, user_dashboard, admin_dashboard
 from member.views import add_member, edit_member
+
+from member.models import Profile
 
 urlpatterns = patterns('',
     url(r'^accounts/', include('registration.backends.default.urls')),
@@ -10,7 +14,16 @@ urlpatterns = patterns('',
     url(r'^dashboard/user/$', user_dashboard, name='user_dashboard'),
     url(r'^dashboard/admin/$', admin_dashboard, name='admin_dashboard'),
     url(r'^member/add/$', add_member, name='add_member'),
+    url(r'^member/$', edit_member, name='member'),
     url(r'^member/edit/(?P<member_id>\d+)/$', edit_member, name='edit_member'),
-    (r'^search/', include('haystack.urls')),
+    url(r'^member/detail/(?P<pk>\d+)/$',
+        DetailView.as_view(
+            model=Profile,
+            context_object_name='member',
+            template_name='member/detail.html'),
+        name='detail_member'),
+##    url(r'^invoice/$', invoice_dashboard, name='invoice_dashboard'),
+##    url(r'^invoice/(?P<member_id>\d+)/$', user_invoice, name='user_invoice'),
+    url(r'^search/', include('haystack.urls')),
 )
 urlpatterns += staticfiles_urlpatterns()
